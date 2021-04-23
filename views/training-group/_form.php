b021bf8489d80f4571a77050ccb75f388ad9e268
Create Studio <rd.sviper@gmail.com>
2021-04-23 19:50:46 +0400

    23-04-2021 fix

--- views/training-group/_form.php
+++ views/training-group/_form.php
@@ -252,7 +252,7 @@ $this->registerJs($js, \yii\web\View::POS_LOAD);
                     {
                         echo '<table class="table table-bordered">';
                         echo '<tr><td><b>Дата</b></td><td><b>Время начала</b></td><td><b>Время окончания</b></td><td><b>Помещение</b></td></tr>';
-                        foreach ($extEvents  as $extEvent) {
+                        foreach ($extEvents as $extEvent) {
                             $class = 'default';
                             if (count($extEvent->checkValideTime($model->id)) > 0 || (strtotime($extEvent->lesson_end_time) - strtotime($extEvent->lesson_start_time)) / 60 < $extEvent->duration * 40 || $extEvent->lesson_date < $model->start_date || $extEvent->lesson_date > $model->finish_date) $class = 'danger';
                             echo '<tr class='.$class.'><td><h5>'.date('d.m.Y', strtotime($extEvent->lesson_date)).'</h5></td><td><h5>'.substr($extEvent->lesson_start_time, 0, -3).'</h5></td><td><h5>'.substr($extEvent->lesson_end_time, 0, -3).'</h5></td><td><h5>'.$extEvent->auditorium->fullName.'</h5></td><td>&nbsp;'.Html::a('Удалить', \yii\helpers\Url::to(['training-group/delete-lesson', 'id' => $extEvent->id, 'modelId' => $model->id]), ['class' => 'btn btn-danger']).'</td></tr>';
