ae01629bebb6eb1e6d63bf949a956688ac713730
Create Studio <rd.sviper@gmail.com>
2021-05-13 17:50:08 +0400

    12-05-2021 fix

--- models/SearchForeignEventParticipants.php
+++ models/SearchForeignEventParticipants.php
@@ -43,7 +43,7 @@ class SearchForeignEventParticipants extends ForeignEventParticipants
         $query = ForeignEventParticipants::find();
         if ($sort == 1)
         {
-            $query = ForeignEventParticipants::find()->where(['!=', 'is_true', 1])->orWhere(['!=', 'guaranted_true', 1])->orWhere(['sex' => 'Другое']);
+            $query = ForeignEventParticipants::find()->where(['is_true' => 0])->andWhere(['!=', 'guaranted_true', 1])->orWhere(['sex' => 'Другое']);
         }
 
         // add conditions that should always apply here
