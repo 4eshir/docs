225820f7dccd6119e8a204be1ff8429d2083a623
work <work@T9-4.RST.EDU>
2021-06-24 16:53:57 +0400

    24-06-2021 add-material

--- views/people-material-object/index.php
+++ views/people-material-object/index.php
@@ -29,6 +29,7 @@ $this->params['breadcrumbs'][] = $this->title;
             'id',
             'people_id',
             'material_object_id',
+            'acceptance_date',
 
             ['class' => 'yii\grid\ActionColumn'],
         ],
