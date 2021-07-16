3600ac9c41dc0d5ca6f8d8748a2de800a9ee6b90
4eshir <g.kalashnik@mail.ru>
2021-07-16 14:59:00 +0400

    16-07-2021 fix people events

--- models/work/TrainingGroupWork.php
+++ models/work/TrainingGroupWork.php
@@ -346,6 +346,7 @@ class TrainingGroupWork extends TrainingGroup
             }
 
         }
+
         $partsArr = [];
         if ($this->participants !== null && $this->participants[0]->participant_id !== "")
         {
@@ -464,14 +465,15 @@ class TrainingGroupWork extends TrainingGroup
                 $theme = LessonThemeWork::find()->where(['training_group_lesson_id' => $lessons[$i]->id])->andWhere(['teacher_id' => $teachers[0]->teacher_id])->one();
                 if ($theme !== null) $counter++;
             }
-
+            var_dump(count($lessons) === count($tp));
+            var_dump($counter);
             if (count($lessons) === count($tp) && $counter == 0)
             {
                 for ($i = 0; $i < count($tp); $i++)
                 {
-                    $theme = LessonTheme::find()->where(['training_group_lesson_id' => $lessons[$i]->id])->andWhere(['teacher_id' => $teachers[0]->teacher_id])->one();
+                    $theme = LessonThemeWork::find()->where(['training_group_lesson_id' => $lessons[$i]->id])->andWhere(['teacher_id' => $teachers[0]->teacher_id])->one();
                     if ($theme === null)
-                        $theme = new LessonTheme();
+                        $theme = new LessonThemeWork();
                     $theme->theme = $tp[$i]->theme;
                     $theme->training_group_lesson_id = $lessons[$i]->id;
                     $theme->teacher_id = $teachers[0]->teacher_id;
