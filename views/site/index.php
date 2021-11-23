e85880b61fd34586432f3db959f69224f5a086d8
work <work@T9-4.RST.EDU>
2021-11-23 09:07:46 +0400

    18-11-2021 fix

--- views/site/index.php
+++ views/site/index.php
@@ -264,7 +264,7 @@ $this->title = 'ЦСХД';
                         <ul>
                             <?php
                                     echo '<li>'.Html::a('Отчеты по обучающимся', \yii\helpers\Url::to(['report/man-hours-report'])).'</li>';
-                                    echo '<li>'.Html::a('Полезные функции', \yii\helpers\Url::to(['report/useful-side-report'])).'</li>';
+                                    //echo '<li>'.Html::a('Полезные функции', \yii\helpers\Url::to(['report/useful-side-report'])).'</li>';
                             ?>
                         </ul>
                     </div>
