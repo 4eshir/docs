1716d73b61d1e5eed52b50d4150506950963f58c
4eshir <g.kalashnik@mail.ru>
2021-11-24 11:36:16 +0400

    24-11-2021 fix message errors

--- models/work/ErrorsWork.php
+++ models/work/ErrorsWork.php
@@ -79,7 +79,7 @@ class ErrorsWork extends Errors
 
     private function ErrorsToTrainingProgram($user, $actual)
     {
-        var_dump($user->aka);
+        var_dump(PeopleWork::find()->where(['id' => $user->aka])->one()->branch->id);
         $result = '';
         $programs = '';
         if (\app\models\components\RoleBaseAccess::CheckSingleAccess(Yii::$app->user->identity->getId(), 16))
