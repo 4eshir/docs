af913344b05eac9222c587859eb70595478caf2b
4eshir <g.kalashnik@mail.ru>
2021-12-01 16:54:28 +0400

    01-12-2021 add color in foreign event

--- models/work/ForeignEventWork.php
+++ models/work/ForeignEventWork.php
@@ -108,9 +108,12 @@ class ForeignEventWork extends ForeignEvent
 
     public function getColor($participant_id, $branch_id, $event_finish_date)
     {
-        var_dump('l');
-        /*$groupsParticipant = TrainingGroupParticipantWork::find()->where(['participant_id' => $participant_id])->all();
+
+        $groupsParticipant = TrainingGroupParticipantWork::find()->where(['participant_id' => $participant_id])->all();
+        var_dump($groupsParticipant);
         $groupSet = TrainingGroupWork::find();
+        var_dump($groupSet);
+        var_dump('l');
         $now = $event_finish_date;
         $flag = false;
         foreach ($groupsParticipant as $groupParticipant)
@@ -120,7 +123,7 @@ class ForeignEventWork extends ForeignEvent
             {
                 $flag = true;
             }
-        }*/
+        }
         if (false === false)
             return 'style = "background-color: #FCF8E3; margin: 0; whi"';
         else
