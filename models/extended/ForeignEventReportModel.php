60ac289c23d08c45390ee35fbb520dafca00164f
work <work@T9-4.RST.EDU>
2021-12-14 15:59:20 +0400

    14-12-2021 nothing

--- models/extended/ForeignEventReportModel.php
+++ models/extended/ForeignEventReportModel.php
@@ -125,7 +125,7 @@ class ForeignEventReportModel extends \yii\base\Model
 
                 $counter1 += count($achieves1) + $counterTeamPrizes;
                 $counter2 += count($achieves2) + $counterTeamWinners;
-                $counterPart1 += count(TeacherParticipantWork::find()->where(['foreign_event_id' => $event->id])->andWhere(['NOT IN', 'participant_id', $tpIds])->all());
+                $counterPart1 += count(TeacherParticipantWork::find()->where(['foreign_event_id' => $event->id])->andWhere(['NOT IN', 'participant_id', $tpIds])->all()) + $counterTeam;
 
                 //ОТЛАДКА
                 $s1 = count($achieves1) + $counterTeamPrizes;
