06872a8e1a3e0a385dabbc9bd5c4647166761838
work <work@T9-4.RST.EDU>
2021-12-15 11:53:37 +0400

    15-12-2021 nothing

--- models/extended/ForeignEventReportModel.php
+++ models/extended/ForeignEventReportModel.php
@@ -290,6 +290,8 @@ class ForeignEventReportModel extends \yii\base\Model
                 $counterPart1 += count(TeacherParticipantWork::find()->where(['foreign_event_id' => $event->id])->andWhere(['NOT IN', 'participant_id', $tpIds])->all()) + $counterTeam;
                 $allTeams += $counterTeam;
 
+                var_dump($counterTeam);
+
                 //ОТЛАДКА
                 $teams = TeamWork::find()->select('name')->distinct()->where(['foreign_event_id' => $event->id])->all();
                 $s1 = count($achieves1) + $counterTeamPrizes;
