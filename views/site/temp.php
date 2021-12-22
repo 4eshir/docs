c186d5006a2dfa2a9d94f6efb1c9e56e91750acb
4eshir <g.kalashnik@mail.ru>
2021-12-22 18:10:35 +0400

    22-12-2021 fix

--- views/site/temp.php
+++ views/site/temp.php
@@ -20,6 +20,30 @@
             margin-right: 30px;
             border: 5px solid black;
         }
+        
+        #orga {
+            height: 50px;
+            width: 280px;
+            border-radius: 20px;
+            border: 2px solid red;
+            background: red;
+        }
+        
+        #orgaGay {
+            height: 50px;
+            width: 280px;
+            border-radius: 20px;
+            border: 2px solid red;
+            background: gray;
+        }
+        
+        #orgaGreen {
+            height: 50px;
+            width: 280px;
+            border-radius: 20px;
+            border: 2px solid red;
+            background: green;
+        }
 
         img {
             position: absolute;
@@ -31,17 +55,19 @@
 </head>
 <body>
     <div style="display: flex">
-        <div id="dropbox"></div>
-        <div id="dropbox"></div>
-        <div id="dropbox"></div>
+        <div id="dropbox1"></div>
+        <div id="dropbox2"></div>
+        <div id="dropbox3"></div>
     </div>
     
-    <img id="gif" src="https://media.giphy.com/media/3ohzdNF74rFtPMZzck/giphy.gif">
+    <div id="pidor" class="orga">
+        <p style="color: black">ГБОУ "АТЛ"</p>
+    </div>
 </body>
 <script>
     window.onload = function () {
     //select the thing we wanna drag
-    var mustachio = document.getElementById('gif');
+    var mustachio = document.getElementById('pidor');
     //listen to the touchmove event, every time it fires, grab the location of the touch
     //then assign it to mustachio
     mustachio.addEventListener('touchmove', function (ev) {
@@ -55,8 +81,29 @@
         //current mustachio position when dropped
         var x = parseInt(mustachio.style.left);
         var y = parseInt(mustachio.style.top);
-        let elem = document.getElementById("dropbox");
+        let elem = document.getElementById("dropbox1");
         elem.innerHTML = "<p>" + x + " " + y + "</p>";
+        if (x > 430)
+        {
+            elem.classList.remove('orga');
+            elem.classList.remove('orgaGray');
+            elem.classList.remove('orgaGreen');
+            elem.classList.add('orgaGray');
+        }
+        if (x > 800)
+        {
+            elem.classList.remove('orga');
+            elem.classList.remove('orgaGray');
+            elem.classList.remove('orgaGreen');
+            elem.classList.add('orgaGreen');
+        }
+        if (x < 430)
+        {
+            elem.classList.remove('orga');
+            elem.classList.remove('orgaGray');
+            elem.classList.remove('orgaGreen');
+            elem.classList.add('orga');
+        }
         //check to see if that position meets our constraints
     })
 }
