c970b32b555e1dd773e60d697c64b59e758c9737
work <work@T9-4.RST.EDU>
2022-01-25 09:51:56 +0400

    25-01-2022 fix

--- views/report/report-result.php
+++ views/report/report-result.php
@@ -25,8 +25,6 @@ $this->title = 'Отчет';
 
         $session = Yii::$app->session;
 
-        var_dump($model->debugInfo2);
-        var_dump($model->debugInfo);
 
         if (strlen($model->debugInfo2) > 150)
         {
