182bf68bb9b06c5933916bc99bf3e557cd686165
work <work@T9-4.RST.EDU>
2022-01-26 16:27:48 +0400

    25-01-2022 fix

--- models/extended/ManHoursReportModel.php
+++ models/extended/ManHoursReportModel.php
@@ -217,9 +217,11 @@ class ManHoursReportModel extends \yii\base\Model
                 {
 
                     $part = TrainingGroupParticipantWork::find()->where(['participant_id' => $part->participant_id])->andWhere(['IN', 'training_group_id', $groupsId])->one();
-                    var_dump($part->id);
+
                     $debug2 .= $part->participantWork->fullName.";".$part->trainingGroupWork->number.";".$part->trainingGroupWork->start_date.";".$part->trainingGroupWork->finish_date.
                          ";".$part->trainingGroupWork->pureBranch.";".$part->participantWork->sex.";".$part->participantWork->birthdate.";1\r\n";
+                    var_dump($debug2);
+                    var_dump('<br>');
                 }
                 $debug2 .= "\r\n";
                 //----------------
