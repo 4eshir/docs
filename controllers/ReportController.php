cced01c1acd07b858384be9beb1bc5ff8c0c4f9d
work <work@T9-4.RST.EDU>
2022-01-26 17:11:22 +0400

    25-01-2022 fix

--- controllers/ReportController.php
+++ controllers/ReportController.php
@@ -151,9 +151,7 @@ class ReportController extends Controller
         if ($session->get('csv2') === null) $session->remove('csv2');
         if ($session->get('csv3') === null) $session->remove('csv3');
 
-        var_dump($temp);
-
-        return '1;2;3';
+        return $data;
     }
 
 
