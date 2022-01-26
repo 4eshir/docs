35dfd8752a0590baafdb54456e33c9538418a5a0
work <work@T9-4.RST.EDU>
2022-01-26 17:16:00 +0400

    25-01-2022 fix

--- controllers/ReportController.php
+++ controllers/ReportController.php
@@ -145,7 +145,7 @@ class ReportController extends Controller
         header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
         header('Pragma: public');
         header('Content-Length: ' . mb_strlen($data));
-        $temp = iconv('utf-8', 'windows-1251', $data);
+        $temp = iconv('utf-16', 'windows-1251', $data);
 
         if ($session->get('csv1') === null) $session->remove('csv1');
         if ($session->get('csv2') === null) $session->remove('csv2');
