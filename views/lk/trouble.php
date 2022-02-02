2e8f046cee92bc9a6960040467fa8c1670003bc3
work <work@T9-4.RST.EDU>
2022-02-02 10:27:28 +0400

    02-02-2022 fix-tokens

--- views/lk/trouble.php
+++ views/lk/trouble.php
@@ -10,11 +10,6 @@ use app\models\work\UserWork;
 //$this->title = $model->people->secondname.' '.$model->responsibilityType->name;
 ?>
 
-<?php
-$access = [12, 13, 14];
-$isMethodist = \app\models\common\AccessLevel::find()->where(['user_id' => Yii::$app->user->identity->getId()])->andWhere(['in', 'access_id', $access])->one();
-?>
-
 <div style="width:100%; height:1px; clear:both;"></div>
 <div>
     <?= $this->render('menu') ?>
