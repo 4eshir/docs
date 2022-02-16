9305f07ed1ea384090371487c23da59be581ae06
4eshir <g.kalashnik@mail.ru>
2022-02-16 12:18:33 +0400

    16-02-2022 add new function

---
+++ models/work/EventErrorsWork.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\models\work;
+
+use app\models\common\EventErrors;
+use Yii;
+
+
+class EventErrorsWork extends EventErrors
+{
+
+}
---
+++ models/work/ForeignEventErrorsWork.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\models\work;
+
+use app\models\common\ForeignEventErrors;
+use Yii;
+
+
+class ForeignEventErrorsWork extends ForeignEventErrors
+{
+    
+}
