e0ab7f64c479257be35d15e1b4a5f3acd6f25cf3
Create Studio <rd.sviper@gmail.com>
2022-02-25 09:56:57 +0400

    25-02-2022 fix

--- controllers/DocumentInController.php
+++ controllers/DocumentInController.php
@@ -48,7 +48,7 @@ class DocumentInController extends Controller
         $session = Yii::$app->session;
         if ($archive !== null && $type !== null)
             $session->set("archive", "1");
-        else
+        if ($archive === null && $type === null)
             $session->remove("archive");
 
         $searchModel = new SearchDocumentIn($archive);
