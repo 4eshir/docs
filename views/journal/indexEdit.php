68eb665d3e2fa45a07a70817abf56a05f2f0c9a6
Create Studio <rd.sviper@gmail.com>
2022-03-09 22:25:42 +0400

    07-03-2022 add-event-way

--- views/journal/indexEdit.php
+++ views/journal/indexEdit.php
@@ -27,7 +27,7 @@ $this->params['breadcrumbs'][] = $this->title;
         for (let i = 0; i < lessons.length; i++) {
             if (lessons[i].parentNode.parentNode.style.background !== 'rgb(145, 138, 138)')
             {
-                console.log(lessons[i].parentNode.parentNode.style.background + ' true');
+                console.log(lessons[i].parentNode.parentNode.style.background);
                 lessons[i].value = "0";
                 lessons[i].style.background = "green";
             }
