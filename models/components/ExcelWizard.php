fd0c2aad1c5233c025e3a6d09d09c7276b8fb786
Create Studio <rd.sviper@gmail.com>
2022-03-19 00:06:13 +0400

    18-03-2022 reports

--- models/components/ExcelWizard.php
+++ models/components/ExcelWizard.php
@@ -724,8 +724,6 @@ class ExcelWizard
         $gIds = [];
         foreach ($groups as $group) $gIds[] = $group->id;
 
-        if ($focus_id == 0)
-            var_dump($gIds);
 
         return $gIds;
     }
