b7ecb1780118e396a1932d74a8c504ca45b11371
Create Studio <rd.sviper@gmail.com>
2022-03-19 00:05:40 +0400

    18-03-2022 reports

--- models/components/ExcelWizard.php
+++ models/components/ExcelWizard.php
@@ -811,7 +811,6 @@ class ExcelWizard
 
         $visits = VisitWork::find()->joinWith(['trainingGroupLesson trainingGroupLesson'])->where(['IN', 'trainingGroupLesson.training_group_id', ExcelWizard::GetGroupsByDatesAndBranch($start_date, $end_date, 2)])->andWhere(['IN', 'visit.id', (new Query())->select('visit.id')->from('visit')->where(['status' => 0])->orWhere(['status' => 2])])->all();
 
-        var_dump(VisitWork::find()->joinWith(['trainingGroupLesson trainingGroupLesson'])->where(['IN', 'trainingGroupLesson.training_group_id', ExcelWizard::GetGroupsByDatesAndBranch($start_date, $end_date, 2)])->andWhere(['IN', 'visit.id', (new Query())->select('visit.id')->from('visit')->where(['status' => 0])->orWhere(['status' => 2])])->createCommand()->getRawSql());
 
         $inputData->getSheet(2)->setCellValueByColumnAndRow(10, 8, count($visits));
 
