b7c9e732f4e95786fcb7ba0529832c120ce2ecbf
work <work@T9-4.RST.EDU>
2022-03-23 10:48:42 +0400

    23-03-2022 fix-ec

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -193,11 +193,20 @@ class SiteController extends Controller
 
     public function actionTemp()
     {
-        $sqlStr = "CREATE EVENT `token_event` ON SCHEDULE AT '2022-01-26 15:25:00' ON COMPLETION NOT PRESERVE ENABLE DO INSERT INTO `log`(`user_id`, `text`, `date`, `time`) VALUES (1, 'Test Event', CURRENT_DATE, CURRENT_TIME)";
-        Yii::$app->db->createCommand($sqlStr)->execute();
-        //$sqlStr = "CREATE EVENT `token_event` ON SCHEDULE AT '2022-01-26 15:25:00' ON COMPLETION NOT PRESERVE ENABLE DO INSERT INTO `log`(`user_id`, `text`, `date`, `time`) VALUES (1, 'Test Event', CURRENT_DATE, CURRENT_TIME)";
-        //Yii::$app->db->createCommand($sqlStr)->execute();
+        $oldBranch = TeacherParticipantWork::find()->all();
+        foreach ($oldBranch as $branch) 
+        {
+            $newBranch = TeacherParticipantBranchWork::find()->where(['teacher_participant_id' => $oldBranch->id])->one();
+            if ($newBranch == null)
+            {
+                $newBranch = new TeacherParticipantBranchWork();
+                $newBranch->branch_id = $oldBranch->branch_id;
+                $newBranch->teacher_participant_id = $oldBranch->id;
+                $newBranch->save();
+            }
+        }
     }
+
 
 
     /*public function beforeAction($action)
