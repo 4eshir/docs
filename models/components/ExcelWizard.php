795e755aa6adda26cbf4d59dbf9214d81e31bf82
work <work@T9-4.RST.EDU>
2022-03-23 15:02:44 +0400

    23-03-2022 fix

--- models/components/ExcelWizard.php
+++ models/components/ExcelWizard.php
@@ -262,6 +262,8 @@ class ExcelWizard
 
         var_dump(TeacherParticipantBranchWork::find()->joinWith(['teacherParticipant teacherParticipant'])->where(['IN', 'teacherParticipant.foreign_event_id', $eIds])->andWhere(['teacher_participant_branch.branch_id' => $branch_id])->andWhere(['teacherParticipant.focus' => $focus_id])->createCommand()->getRawSql());
 
+        var_dump($events1);
+
 
         $counter1 = 0;
         $counter2 = 0;
