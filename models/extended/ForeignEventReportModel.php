76902eff864cb319ee4dc16f1b462853252b93f6
Create Studio <rd.sviper@gmail.com>
2022-04-01 14:01:02 +0400

    01-04-2022 fix

--- models/extended/ForeignEventReportModel.php
+++ models/extended/ForeignEventReportModel.php
@@ -67,7 +67,7 @@ class ForeignEventReportModel extends \yii\base\Model
 
         $pIds = [];
         foreach ($participants as $participant) $pIds[] = $participant->participant_id;
-        $eventParticipants = TeacherParticipantWork::find()->where(['IN', 'participant_id', $pIds])->andWhere(['IN', 'branch_id', $this->branch])->all();
+        $eventParticipants = TeacherParticipantWork::find()->joinWith(['teacherParticipantBranch teacherParticipantBranch'])->where(['IN', 'participant_id', $pIds])->andWhere(['IN', 'teacherParticipantBranch.branch_id', $this->branch])->all();
 
 
         $eIds = [];
