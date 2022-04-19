b8553a252d8d65cb40da547e3cd8eadf7824cfe2
Create Studio <rd.sviper@gmail.com>
2022-04-19 22:02:36 +0400

    17-04-2022

--- models/extended/ManHoursReportModel.php
+++ models/extended/ManHoursReportModel.php
@@ -319,6 +319,8 @@ class ManHoursReportModel extends \yii\base\Model
 
                 foreach ($parts as $part) $checkParticipantsId[] = $part->participant_id;
 
+                var_dump($checkParticipantsId);
+
                 $result .= '<tr><td><b>4</b></td><td>Количество обучающихся, начавших обучение до '.$this->start_date.' и завершивших после '.$this->end_date.'</td><td>'.count($parts). ' чел.'.'</td></tr>';
 
                 //ОТЛАДОЧНЫЙ ВЫВОД
