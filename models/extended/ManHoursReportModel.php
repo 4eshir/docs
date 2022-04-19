3ee672e37cd07972a9679a30394717106a87d6ac
Create Studio <rd.sviper@gmail.com>
2022-04-19 22:09:20 +0400

    17-04-2022

--- models/extended/ManHoursReportModel.php
+++ models/extended/ManHoursReportModel.php
@@ -291,6 +291,7 @@ class ManHoursReportModel extends \yii\base\Model
                     $parts = TrainingGroupParticipantWork::find()->where(['IN', 'training_group_id', $groupsId])->all();
 
                 foreach ($parts as $part) $checkParticipantsId[] = $part->participant_id;
+                var_dump($checkParticipantsId);
 
                 $result .= '<tr><td><b>3</b></td><td>Количество обучающихся, начавших обучение после '.$this->start_date.' и завершивших до '.$this->end_date.'</td><td>'.count($parts). ' чел.'.'</td></tr>';
 
@@ -323,6 +324,7 @@ class ManHoursReportModel extends \yii\base\Model
                     $parts = TrainingGroupParticipantWork::find()->where(['IN', 'training_group_id', $groupsId])->all();
 
                 foreach ($parts as $part) $checkParticipantsId[] = $part->participant_id;
+                var_dump($checkParticipantsId);
 
                 $result .= '<tr><td><b>4</b></td><td>Количество обучающихся, начавших обучение до '.$this->start_date.' и завершивших после '.$this->end_date.'</td><td>'.count($parts). ' чел.'.'</td></tr>';
 
