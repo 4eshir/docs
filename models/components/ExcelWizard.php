72d4934ce0c903764d0c0fd23e054da61cc898fc
work <work@T9-4.RST.EDU>
2022-08-17 15:27:11 +0400

    17-08-2022 edit-foreign-events

--- models/components/ExcelWizard.php
+++ models/components/ExcelWizard.php
@@ -397,7 +397,7 @@ class ExcelWizard
             }
 
             if ($branch_id == 7 && $focus_id == 5)
-                var_dump($teams);
+                var_dump(TeamWork::find()->where(['foreign_event_id' => $event->id])->andWhere(['IN', 'participant_id', $eIds])->createCommand()->getRawSql());
 
 
             $tpIds = [];
