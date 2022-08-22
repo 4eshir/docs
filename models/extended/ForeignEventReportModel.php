d4f54e9994b2200d4c3f08be3de5e4c29b1ba787
work <work@T9-4.RST.EDU>
2022-08-22 11:31:08 +0400

    22-08-2022 fix-reports

--- models/extended/ForeignEventReportModel.php
+++ models/extended/ForeignEventReportModel.php
@@ -86,8 +86,6 @@ class ForeignEventReportModel extends \yii\base\Model
 
         
 
-        //var_dump(count($events->all()));
-
         //-------------------------------------------
 
         //======РЕЗУЛЬТАТ======
