047be180f7e4d9b9f8e0b850a13628234fd5c1f4
work <work@T9-4.RST.EDU>
2022-10-21 14:58:51 +0400

    21-10-2022

--- views/training-group/_form.php
+++ views/training-group/_form.php
@@ -1071,7 +1071,9 @@ $isMethodist = \app\models\work\UserRoleWork::find()->where(['user_id' => Yii::$
         </div>
 
         <div>
-            <?php if (RoleBaseAccess::CheckRole(Yii::$app->user->identity->getId(), 5) || RoleBaseAccess::CheckRole(Yii::$app->user->identity->getId(), 6) || RoleBaseAccess::CheckRole(Yii::$app->user->identity->getId(), 7))
+            <?php 
+            var_dump(RoleBaseAccess::CheckRole(70, 5).' '.RoleBaseAccess::CheckRole(70, 6).' '.RoleBaseAccess::CheckRole(70, 7));
+            if (RoleBaseAccess::CheckRole(Yii::$app->user->identity->getId(), 5) || RoleBaseAccess::CheckRole(Yii::$app->user->identity->getId(), 6) || RoleBaseAccess::CheckRole(Yii::$app->user->identity->getId(), 7))
                 echo $form->field($model, 'protection_confirm')->checkbox(); ?>
         </div>
     </div>
