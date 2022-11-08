e8efbc2d34c63e5c96adc07481636a4168769865
work <work@T9-4.RST.EDU>
2022-11-08 13:20:58 +0400

    08-11-2022 fix

--- models/extended/ForeignEventReportModel.php
+++ models/extended/ForeignEventReportModel.php
@@ -123,11 +123,6 @@ class ForeignEventReportModel extends \yii\base\Model
 
             if ($partsLink !== null)
             {
-                if ($event->id == 295)
-                {
-                    var_dump(ParticipantAchievementWork::find()->where(['foreign_event_id' => $event->id])->andWhere(['NOT IN', 'participant_id', $tpIds])->andWhere(['winner' => 0])->andWhere(['IN', 'participant_id', $pIds])->createCommand()->getRawSql());
-                    var_dump(count($pIds));
-                }
 
                 if ($events_id2 == 0)
                 {
