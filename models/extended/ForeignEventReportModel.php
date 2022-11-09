155fcabf00e5c7afdfacf671a5c9841a4f1af0a3
work <work@T9-4.RST.EDU>
2022-11-09 14:05:50 +0400

    09-11-2022 words

--- models/extended/ForeignEventReportModel.php
+++ models/extended/ForeignEventReportModel.php
@@ -166,7 +166,7 @@ class ForeignEventReportModel extends \yii\base\Model
             $teamStr = count($teams) > 0 ? ' (в т.ч. команды - '.count($teams).')' : '';
             $teamPrizeStr = $counterTeamPrizes > 0 ? ' (в т.ч. команды - '.$counterTeamPrizes.')' : '';
             $teamWinnersStr = $counterTeamWinners > 0 ? ' (в т.ч. команды - '.$counterTeamWinners.')' : '';
-            $debug .= (count(TeacherParticipantWork::find()->joinWith(['teacherParticipantBranches teacherParticipantBranches'])->where(['foreign_event_id' => $event->id])->andWhere(['NOT IN', 'participant_id', $tpIds])->andWhere(['IN', 'teacherParticipantBranches.branch_id', $branch_id])->andWhere(['IN', 'allow_remote_id', $allow_remote_id])->all()).";".$counterTeam.";".$s1.$teamPrizeStr.";".$s2. $teamWinnersStr."\r\n";
+            $debug .= count(TeacherParticipantWork::find()->joinWith(['teacherParticipantBranches teacherParticipantBranches'])->where(['foreign_event_id' => $event->id])->andWhere(['NOT IN', 'participant_id', $tpIds])->andWhere(['IN', 'teacherParticipantBranches.branch_id', $branch_id])->andWhere(['IN', 'allow_remote_id', $allow_remote_id])->all()).";".$counterTeam.";".$s1.$teamPrizeStr.";".$s2. $teamWinnersStr."\r\n";
             //ОТЛАДКА
 
         }
