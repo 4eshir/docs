dfad839a7b820021268aa0e3cee59b71d2662607
work <work@T9-4.RST.EDU>
2022-11-10 17:02:46 +0400

    09-11-2022 words

--- models/components/ExcelWizard.php
+++ models/components/ExcelWizard.php
@@ -1221,16 +1221,9 @@ class ExcelWizard
 
         //var_dump($all);
         
-        if ($all == 0) return 0;
+        if ($winners1[4] + $winners2[4] + $winners3[4] == 0) return 0;
 
-        if ($branch_id == 3 && $focus_id == 3)
-        {
-            var_dump(($winners1[3] + $winners2[3] + $winners3[3]));
-            var_dump(($winners1[4] + $winners2[4] + $winners3[4]));
-            var_dump(round(($winners1[3] + $winners2[3] + $winners3[3]) / ($winners1[4] + $winners2[4] + $winners3[4]) * 100));
-        }
-
-        return round((($winners1[3] + $winners2[3] + $winners3[3]) / ($winners1[4] + $winners2[4] + $winners3[4])) * 100);
+        return round(($winners1[3] + $winners2[3] + $winners3[3]) / ($winners1[4] + $winners2[4] + $winners3[4]) * 100);
         //return round((($winners1[0] + $winners1[1] + $winners2[0] + $winners2[1] + $winners3[0] + $winners3[1]) / $all) * 100);
     }
 
