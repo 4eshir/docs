1ab0fd556d79d280145dd60324df00a430b5bdd0
work <work@T9-4.RST.EDU>
2022-12-14 10:24:35 +0400

    12-12-2022

--- models/extended/ManHoursReportModel.php
+++ models/extended/ManHoursReportModel.php
@@ -368,7 +368,7 @@ class ManHoursReportModel extends \yii\base\Model
 
                 $result .= '<tr><td><b>4</b></td><td>Количество обучающихся, начавших обучение до '.$this->start_date.' и завершивших после '.$this->end_date.'</td><td>'.count($parts). ' чел.'.'</td></tr>';
 
-                var_dump($parts[0]->participantWork->fullName);
+                //var_dump($parts[0]->participantWork->fullName);
 
 
                 //ОТЛАДОЧНЫЙ ВЫВОД
@@ -385,7 +385,7 @@ class ManHoursReportModel extends \yii\base\Model
                     $strTeacher = substr($strTeacher, 0, -1);
 
                     $debug2 .= $part->participantWork->fullName.";".$part->trainingGroupWork->number.";".$part->trainingGroupWork->start_date.";".$part->trainingGroupWork->finish_date.
-                         ";".$part->trainingGroupWork->pureBranch.";".$part->participantWork->sex.";".$part->participantWork->birthdate.";".$part->trainingGroupWork->trainingProgramWork->focusWork->name.";".$strTeacher.";".$part->trainingGroupWork->budgetText.";".$part->trainingGroupWork->trainingProgramWork->thematicDirectionWork->full_name.";".$part->trainingGroupWork->trainingProgramWork->name.";".$part->groupProjectThemesWork->projectThemeWork->name.";".explode(" ", $part->trainingGroupWork->protection_date)[0].";".$part->trainingGroupWork->groupProjectThemesWork->projectTypeWork->name.";4\r\n";
+                         ";".$part->trainingGroupWork->pureBranch.";".$part->participantWork->sex.";".$part->participantWork->birthdate.";".$part->trainingGroupWork->trainingProgramWork->focusWork->name.";".$strTeacher.";".$part->trainingGroupWork->budgetText.";".$part->trainingGroupWork->trainingProgramWork->thematicDirectionWork->full_name.";".$part->trainingGroupWork->trainingProgramWork->name.";".$part->groupProjectThemesWork->projectThemeWork->name.";".explode(" ", $part->trainingGroupWork->protection_date)[0].";".$part->groupProjectThemesWork->projectTypeWork->name.";4\r\n";
                 }
                 $debug2 .= "\r\n";
                 //----------------
