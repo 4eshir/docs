a84cf7d5136962033120df3028d8c19920b9708a
work <work@T9-4.RST.EDU>
2023-01-31 16:08:52 +0400

    31-01-2022

--- models/common/TrainingProgram.php
+++ models/common/TrainingProgram.php
@@ -35,6 +35,11 @@ use Yii;
  * @property BranchProgram[] $branchPrograms
  * @property ThematicPlan[] $thematicPlans
  * @property TrainingGroup[] $trainingGroups
+ * @property AllowRemote $allowRemote
+ * @property CertificatType $certificatType
+ * @property Focus $focus
+ * @property User $lastUpdate
+ * @property ThematicDirection $thematicDirection
  */
 class TrainingProgram extends \yii\db\ActiveRecord
 {
@@ -57,6 +62,11 @@ class TrainingProgram extends \yii\db\ActiveRecord
             [['ped_council_date'], 'safe'],
             [['description'], 'string'],
             [['name', 'ped_council_number', 'doc_file', 'edit_docs', 'key_words', 'contract'], 'string', 'max' => 1000],
+            [['allow_remote_id'], 'exist', 'skipOnError' => true, 'targetClass' => AllowRemote::className(), 'targetAttribute' => ['allow_remote_id' => 'id']],
+            [['certificat_type_id'], 'exist', 'skipOnError' => true, 'targetClass' => CertificatType::className(), 'targetAttribute' => ['certificat_type_id' => 'id']],
+            [['focus_id'], 'exist', 'skipOnError' => true, 'targetClass' => Focus::className(), 'targetAttribute' => ['focus_id' => 'id']],
+            [['last_update_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['last_update_id' => 'id']],
+            [['thematic_direction_id'], 'exist', 'skipOnError' => true, 'targetClass' => ThematicDirection::className(), 'targetAttribute' => ['thematic_direction_id' => 'id']],
         ];
     }
 
@@ -130,5 +140,55 @@ class TrainingProgram extends \yii\db\ActiveRecord
     public function getTrainingGroups()
     {
         return $this->hasMany(TrainingGroup::className(), ['training_program_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[AllowRemote]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAllowRemote()
+    {
+        return $this->hasOne(AllowRemote::className(), ['id' => 'allow_remote_id']);
+    }
+
+    /**
+     * Gets query for [[CertificatType]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCertificatType()
+    {
+        return $this->hasOne(CertificatType::className(), ['id' => 'certificat_type_id']);
+    }
+
+    /**
+     * Gets query for [[Focus]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getFocus()
+    {
+        return $this->hasOne(Focus::className(), ['id' => 'focus_id']);
+    }
+
+    /**
+     * Gets query for [[LastUpdate]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getLastUpdate()
+    {
+        return $this->hasOne(User::className(), ['id' => 'last_update_id']);
+    }
+
+    /**
+     * Gets query for [[ThematicDirection]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getThematicDirection()
+    {
+        return $this->hasOne(ThematicDirection::className(), ['id' => 'thematic_direction_id']);
     }
 }
