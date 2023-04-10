1b69dc81bdf5fc3f5d4b0320fdda99a2aeb4d67b
work <work@T9-4.RST.EDU>
2023-04-10 15:21:39 +0400

    10-04-2023

--- models/work/MaterialObjectWork.php
+++ models/work/MaterialObjectWork.php
@@ -53,7 +53,7 @@ class MaterialObjectWork extends MaterialObject
         return [
             //[['name', 'price', 'number', 'finance_source_id', 'type', 'is_education'], 'required'],
             [['count', 'finance_source_id', 'type', 'is_education', 'state', 'status', 'write_off', 'expiration_date', 'kind_id', 'amount', 'complex'], 'integer'],
-            [['price'], 'number'],
+            [['price'], 'double'],
             [['lifetime', 'create_date', 'characteristics', 'name', 'price', 'number', 'finance_source_id', 'type', 'is_education', 'filesTmp', 'filesName'], 'safe'],
             [['name', 'photo_local', 'photo_cloud', 'expirationDate'], 'string', 'max' => 1000],
             [['attribute'], 'string', 'max' => 3],
