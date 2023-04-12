c36808746473dbd754fcc405c744ef9d6d560cd1
work <work@T9-4.RST.EDU>
2023-04-12 12:28:39 +0400

    12-04-2023

--- controllers/InvoiceController.php
+++ controllers/InvoiceController.php
@@ -115,7 +115,7 @@ class InvoiceController extends Controller
         $modelObjects = [new MaterialObjectWork];
 
         if ($model->load(Yii::$app->request->post())) {
-            var_dump($_POST["characteristics"]);
+            var_dump($_POST["InvoiceWork"]);
             $modelObjects = DynamicModel::createMultiple(MaterialObjectWork::classname());
             DynamicModel::loadMultiple($modelObjects, Yii::$app->request->post());
             $model->objects = $modelObjects;
