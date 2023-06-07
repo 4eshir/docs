ed935cc1547a9cae39a488c930dcbb3765660678
unknown <Admin@T6-3U.RST.EDU>
2023-06-07 11:26:40 +0400

    07-06-2023

--- commands/DatabaseTestController.php
+++ commands/DatabaseTestController.php
@@ -46,7 +46,7 @@ class DatabaseTestController extends Controller
      */
     public function actionCheckFileAccess()
     {
-        $res = file_exists(iconv('utf-8', 'win-1251', '/var/www/u1471742/data/www/index.schooltech.ru/docs//upload/files/training-program/edit_docs/')) ? '+' : '-';
+        $res = file_exists(iconv('utf-8', 'win-1251', '/var/www/u1471742/data/www/index.schooltech.ru/docs//upload/files/training-program/doc/')) ? '+' : '-';
         $this->stdout($res."\n", Console::FG_GREEN);
 
         /*
