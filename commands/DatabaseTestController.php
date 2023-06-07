95a21074b9b2b181589109c79d9d5461a314fae6
unknown <Admin@T6-3U.RST.EDU>
2023-06-07 11:10:46 +0400

    07-06-2023

--- commands/DatabaseTestController.php
+++ commands/DatabaseTestController.php
@@ -46,8 +46,7 @@ class DatabaseTestController extends Controller
      */
     public function actionCheckFileAccess()
     {
-        $res = file_exists('/var/www/u1471742/data/www/index.schooltech.ru/docs//upload/files/training-program/edit_docs/Ред1_20230515_Олимпиадная_физика_в_экспериментальных_задачах._Вводный_уровень.docx
-') ? '+' : '-';
+        $res = file_exists('sftp://u1471742@37.140.192.82/var/www/u1471742/data/www/index.schooltech.ru/docs/upload/files/training-program/edit_docs/%D0%A0%D0%B5%D0%B41_20230515_%D0%9E%D0%BB%D0%B8%D0%BC%D0%BF%D0%B8%D0%B0%D0%B4%D0%BD%D0%B0%D1%8F_%D1%84%D0%B8%D0%B7%D0%B8%D0%BA%D0%B0_%D0%B2_%D1%8D%D0%BA%D1%81%D0%BF%D0%B5%D1%80%D0%B8%D0%BC%D0%B5%D0%BD%D1%82%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D1%85_%D0%B7%D0%B0%D0%B4%D0%B0%D1%87%D0%B0%D1%85._%D0%92%D0%B2%D0%BE%D0%B4%D0%BD%D1%8B%D0%B9_%D1%83%D1%80%D0%BE%D0%B2%D0%B5%D0%BD%D1%8C.docx') ? '+' : '-';
         $this->stdout($res, Console::FG_GREEN);
 
         /*
