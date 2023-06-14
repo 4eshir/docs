7b35c4fa236274a0eb81fece9925cf58556ab0f2
4eshir <g.kalashnik@mail.ru>
2023-06-14 16:49:34 +0400

    14-06-2023 add new certificat

--- models/components/PdfWizard.php
+++ models/components/PdfWizard.php
@@ -43,9 +43,9 @@ class PdfWizard
     {
         $certificat = CertificatWork::find()->where(['id' => $certificat_id])->one();
         if (strripos($certificat->certificatTemplate->name, 'лето'))
-            PdfWizard::CertificatTechnoSummer($certificat_id, $destination, $path = null);
+            return PdfWizard::CertificatTechnoSummer($certificat_id, $destination, $path = null);
         else
-            PdfWizard::CertificatStandard($certificat_id, $destination, $path = null);
+            return PdfWizard::CertificatStandard($certificat_id, $destination, $path = null);
     }
 
     static private function CertificatStandard ($certificat_id, $destination, $path = null)
