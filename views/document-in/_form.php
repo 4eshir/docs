39db20b2f31fe81a765f44edd48ec5cb988c7bc2
unknown <Admin@T6-3U.RST.EDU>
2023-07-14 11:23:05 +0400

    14-07-2023

--- views/document-in/_form.php
+++ views/document-in/_form.php
@@ -125,7 +125,7 @@ use yii\jui\DatePicker;
         $companyNull = \app\models\work\CompanyWork::find()->where(['id' => 7])->one();
         $items1 = \yii\helpers\ArrayHelper::map($company,'id','name');
         $items2 = \yii\helpers\ArrayHelper::map($companyNull,'id','name');
-        echo $items2[0];
+        echo '<h1>'.$items2[0].'</h1>';
         $items = array_merge($items2, $items1);
         $params = [
 
