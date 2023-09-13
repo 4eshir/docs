791d5e351c51afc878c99a9a87cd7710dcde3e4a
4eshir <g.kalashnik@mail.ru>
2023-09-13 10:17:32 +0400

    13-09-2023 add fix

--- views/document-order/foreign-event.php
+++ views/document-order/foreign-event.php
@@ -215,7 +215,7 @@ $session = Yii::$app->session;
         document.getElementsByClassName('form-group field-documentorderwork-foreign_event-is_minpros')[0].childNodes[4].style.color = 'white';
 
         let url = "<?= Url::toRoute('subsupplement'); ?>";
-        //console.log(window.location.search);
+        console.log(window.location.search);
         console.log(url);
         $.post(
             url,
