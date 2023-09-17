d126693e7b680149277ddd9871ae85b2f00d24db
4eshir <g.kalashnik@mail.ru>
2023-09-18 00:47:32 +0400

    18-09-2023

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -228,6 +228,7 @@ class SiteController extends Controller
     public function actionTemp()
     {
         ini_set('error_reporting', 0);
+        ini_set('display_errors', 0);
         //var_dump($stream->getSize());
         /*$logs = LogWork::find()->where(['like', 'text', 'Добавлена группа%', false])->all();
         foreach ($logs as $log)
