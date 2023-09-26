04c1c8a0771d83ba99a6de8826b8d5083c9bce6d
unknown <Admin@T6-3U.RST.EDU>
2023-09-26 14:07:13 +0400

    26-09-2023

--- models/components/report/DebugReportFunctions.php
+++ models/components/report/DebugReportFunctions.php
@@ -15,26 +15,11 @@ class DebugReportFunctions
      * $visits - массив класса visit (посещения по заданным параметрам)
      * [$start_date : $end_date] - Промежуток для поиска занятий и явок (границы включены)
      */
-    static public function DebugDataManHours($visits, $start_date, $end_date)
+    static public function DebugDataManHours($groups, $visits, $start_date, $end_date)
     {
         $visits = VisitWork::find()->where(['IN', 'id', $visits])->all();
 
         $modelsArr = [new DebugManHoursModel];
-
-        $participantsId = [];
-        $lessonsId = [];
-        foreach ($visits as $visit)
-        {
-            $lessonsId[] = $visit->training_group_lesson_id;
-            $participantsId[] = $visit->foreign_event_participant_id;
-        }
-
-        $lessons = TrainingGroupLessonWork::find()->where(['IN', 'id', $lessonsId])->all();
-
-        $groupsId = [];
-        foreach ($lessons as $lesson) $groupsId[] = $lesson->training_group_id;
-
-        $groups = TrainingGroupWork::find()->where(['IN', 'id', $groupsId])->all();
 
         foreach ($groups as $group)
         {
@@ -43,8 +28,7 @@ class DebugReportFunctions
 
             $lessonAllTemp = TrainingGroupLessonWork::find()->where(['training_group_id' => $group->id])->all();
             $lessonTeacherTemp = TrainingGroupLessonWork::find()->where(['training_group_id' => $group->id])
-                ->andWhere(['IN', 'id', $lessonsId])
-                ->andWhere(['>=', 'lesson_date', $start_date])
+                ->where(['>=', 'lesson_date', $start_date])
                 ->andWhere(['<=', 'lesson_date', $end_date])->all();
 
             $lttIds = SupportReportFunctions::GetIdFromArray($lessonTeacherTemp);
