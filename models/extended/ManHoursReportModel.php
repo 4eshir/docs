b9cdfb89259cfae3ae455c8edec108ed43156e7b
unknown <Admin@T6-3U.RST.EDU>
2023-09-26 13:50:18 +0400

    26-09-2023

--- models/extended/ManHoursReportModel.php
+++ models/extended/ManHoursReportModel.php
@@ -65,7 +65,7 @@ class ManHoursReportModel extends \yii\base\Model
                                                             $this->budget,
                                                             $this->teacher == '' ? [] : $this->teacher);
 
-        $participants = SupportReportFunctions::GetParticipantsFromGroups(ReportConst::PROD, $groups, $this->unic, ReportConst::AGES_ALL_18, date('Y-m-d'));
+        $participants = SupportReportFunctions::GetParticipantsFromGroups(ReportConst::PROD, $groups, $this->unic, ReportConst::AGES_ALL, date('Y-m-d'));
 
         $visits = SupportReportFunctions::GetVisits(ReportConst::PROD, $participants, $this->start_date, $this->end_date, $this->method == 0 ? VisitWork::ONLY_PRESENCE : VisitWork::PRESENCE_AND_ABSENCE/*, $this->teacher == null ? [] : [$this->teacher]*/);
 
