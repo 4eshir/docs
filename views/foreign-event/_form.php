5d16837323d3d5216068121ad9e2684ba893eaa1
4eshir <g.kalashnik@mail.ru>
2023-10-10 11:33:28 +0400

    10-10-2023 add fix

--- views/foreign-event/_form.php
+++ views/foreign-event/_form.php
@@ -302,7 +302,7 @@ use yii\jui\DatePicker;
                                 /*$partsTeam = \app\models\work\TeamWork::find()->joinWith('teacherParticipant teacherParticipant')->where(['teacherParticipant.foreign_event_id' => $model->id])->andWhere(['IS NOT','team_name_id', null])->all();
                                 foreach ($partsTeam as $partTeam)
                                     $partsArr[] = $partTeam->teacher_participant_id;*/
-                                $partsTeam = \app\models\work\TeacherParticipantWork::find()->joinWith(['teams teams'])->where(['teacherParticipant.foreign_event_id' => $model->id])->andWhere(['IS NOT','teams.team_name_id', null])
+                                $partsTeam = \app\models\work\TeacherParticipantWork::find()->joinWith(['teams teams'])->where(['teacher_participant.foreign_event_id' => $model->id])->andWhere(['IS NOT','teams.team_name_id', null])
                                     ->groupBy(['nomination'])->groupBy(['focus'])->groupBy(['teams.team_name_id'])->all();
                                 foreach ($partsTeam as $partTeam)
                                     $partsArr[] = $partTeam->id;
