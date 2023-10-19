8ca37d3a120e821f1aad26afc984768e6a862a86
4eshir <g.kalashnik@mail.ru>
2023-10-19 10:37:38 +0400

    test

--- controllers/DocsOutController.php
+++ controllers/DocsOutController.php
@@ -268,10 +268,10 @@ class DocsOutController extends Controller
 
             $fp = fopen('php://output', 'r');
 
-            //header('Content-Description: File Transfer');
-            //header('Content-Type: application/octet-stream');
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
             header('Content-Disposition: attachment; filename=' . $downloadYadi->filename);
-            //header('Content-Transfer-Encoding: binary');
+            header('Content-Transfer-Encoding: binary');
             header('Content-Length: ' . $downloadYadi->file->size);
 
             $downloadYadi->file->download($fp);
