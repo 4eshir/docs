86265f6cc3f2e8799608392ddcdd5e570c61880f
unknown <Admin@T6-3U.RST.EDU>
2024-04-26 15:39:08 +0400

    test

--- models/components/report/ReportWizard.php
+++ models/components/report/ReportWizard.php
@@ -695,11 +695,11 @@ class ReportWizard
         $ageParticipantsSportDebug = SupportReportFunctions::GetParticipantsFromGroups(ReportConst::PROD, $sportGroups, 1,
             [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17], ((int)explode("-", $start_date)[0] + 1).'-01-01');
 
-        foreach ($ageParticipantsSciDebug as $participant) {
+        /*foreach ($ageParticipantsSciDebug as $participant) {
             $inputData->getSheet(0)->setCellValueByColumnAndRow(0, $counter, $participant->participant->secondname.' '.$participant->participant->firstname.' '.$participant->participant->patronymic);
             $inputData->getSheet(0)->setCellValueByColumnAndRow(1, $counter, 'Научная '.$participant->id);
             $counter++;
-        }
+        }*/
 
         $ageParticipants12 = SupportReportFunctions::GetParticipantsFromGroups(ReportConst::PROD, $scienceGroups, 1, [0, 1, 2], ((int)explode("-", $start_date)[0] + 1).'-01-01');
         var_dump('Людей младше 3 лет: '.count($ageParticipants12));
